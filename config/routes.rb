c789ac56bc2738f2dba4d41a7224a1d3e5849cf3
krec-kiran <krec.kiran@gmail.com>
2017-03-05 20:12:37 +0000

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :biodata
+  resources :biodata, except: [:show]
+  get 'biodata/:id', to:'biodata#show'
+
 
   get 'about', to:'pages#home'
   get 'contact', to:'pages#contact'
