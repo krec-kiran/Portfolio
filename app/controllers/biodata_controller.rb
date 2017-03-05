1224f1975eba12436d868f08b943f6bc2c15dcf5
krec-kiran <krec.kiran@gmail.com>
2017-03-05 15:29:53 +0000

    integrated new & create

--- app/controllers/biodata_controller.rb
+++ app/controllers/biodata_controller.rb
@@ -1,5 +1,22 @@
 class BiodataController < ApplicationController
   def index
-    @portfolio_items = Biodatum.all
+    @portfolio_item = Biodatum.all
   end
+
+  def new
+    @portfolio_item = Biodatum.new
+  end
+
+  def create
+  @portfolio_item = Biodatum.new(params.require(:biodatum).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to biodata_path, notice: 'You portfolio item is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
