a56dde21ab022f986985429ec28aaf09245c61ad
krec-kiran <krec.kiran@gmail.com>
2017-03-05 16:32:59 +0000

    edit & update functionality

--- app/controllers/biodata_controller.rb
+++ app/controllers/biodata_controller.rb
@@ -19,4 +19,20 @@ class BiodataController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Biodatum.find(params[:id])
+  end
+
+  def update
+   @portfolio_item = Biodatum.find(params[:id])
+
+   respond_to do |format|
+      if @portfolio_item.update(params.require(:biodatum).permit(:title, :subtitle, :body))
+        format.html { redirect_to biodata_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
+       end
+    end
+  end
+
 end
