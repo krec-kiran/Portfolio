2453fe14f6e9cd379b86792aef32d54a8933472f
krec-kiran <krec.kiran@gmail.com>
2017-03-08 14:14:05 +0000

    added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :biodata, except: [:show]
 
   get 'angular-items', to:'biodata#angular'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307155745) do
+ActiveRecord::Schema.define(version: 20170308130349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,10 +65,35 @@ ActiveRecord::Schema.define(version: 20170307155745) do
     t.index ["biodatum_id"], name: "index_teches_on_biodatum_id", using: :btree
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "biodata_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["biodata_id"], name: "index_technologies_on_biodata_id", using: :btree
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "blogs", "topics"
