551a5295c695cf3f8214d8e2aa75acc6c2bf2402
krec-kiran <krec.kiran@gmail.com>
2017-03-09 14:13:45 +0000

    new code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  include DeviseWhitelist
+  include SetSource
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :PIN])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :PIN])
-  end
 end
