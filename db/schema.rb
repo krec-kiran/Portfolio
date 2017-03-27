d1564c9d241d5839e67603a0cbcf089ef66cdec7
krec-kiran <krec.kiran@gmail.com>
2017-03-27 22:20:43 +0100

    added email coloumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314182300) do
+ActiveRecord::Schema.define(version: 20170327211940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
